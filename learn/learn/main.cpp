447ed8aea22131d31d97d2b9dacdcdf5d28c4692
“geng” <747720541@qq.com>
2019-03-05 15:15:59 +0800

    First Commit

---
+++ learn/learn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learn
+//
+//  Created by 耿士奇 on 2019/3/5.
+//  Copyright © 2019年 耿士奇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
